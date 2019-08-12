27dbf8633ea8aab1681b092bbca44b8255ba7935
nrrao <nandanarao@yahoo.com>
2019-08-12 16:19:50 -0400

    added yob function

--- nandanaFunctionDrills1.js
+++ nandanaFunctionDrills1.js
@@ -1,6 +1,10 @@
+function getYearOfBirth(age){
+    return 2019-age;
+}
+    
 function createGreeting(name,age) {
-    const yearOfBirth = 2019-age;
-    return `Hi ,my name is ${name} and I'm ${age} years old and I was born in year ${yearOfBirth}`;
+    const yob = getYearOfBirth(age);
+    return `Hi ,my name is ${name} and I'm ${age} years old and I was born in year ${yob}`;
 }
 const greeting1 = createGreeting("nandana",29);
 console.log(greeting1);
