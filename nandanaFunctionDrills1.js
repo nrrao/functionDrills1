255e82750c0b4cb45c36ef6bde7bdb03e1a5f70a
nrrao <nandanarao@yahoo.com>
2019-08-12 17:26:38 -0400

    try/catch

--- nandanaFunctionDrills1.js
+++ nandanaFunctionDrills1.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line strict
 function yearOfBirth(age) {
   if(Math.sign(age) === -1){
@@ -17,8 +18,21 @@ try {
 
     
 function createGreeting(name,age) {
+  if (name === undefined || age === undefined){
+    throw 'Arguments not defined'
+  }
+
+  if ((typeof(name)!= 'string') || (typeof(age)!= 'number')){
+    throw new TypeError('Name should be string and age should be number');
+  }
   const yob = yearOfBirth(age);
   return `Hi ,my name is ${name} and I'm ${age} years old and I was born in year ${yob}`;
+}
+
+try {
+  const greeting2 = createGreeting('','')
+} catch(error){
+  console.log(error);
 }
 const greeting1 = createGreeting('nandana',29);
 console.log(greeting1);
