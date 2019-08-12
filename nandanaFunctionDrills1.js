543d0027fac41bf28bd6ac6b785c95dc52fe1e76
nrrao <nandanarao@yahoo.com>
2019-08-12 16:13:23 -0400

    parameters

--- nandanaFunctionDrills1.js
+++ nandanaFunctionDrills1.js
@@ -1,5 +1,5 @@
-function createGreeting() {
-    return "Hi ,my name is Nandana and I'm 29 years old";
+function createGreeting(name,age) {
+    return `Hi ,my name is ${name} and I'm ${age} years old`;
 }
-const greeting1 = createGreeting();
+const greeting1 = createGreeting("nandana",29);
 console.log(greeting1);
