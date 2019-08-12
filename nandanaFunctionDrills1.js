c0f8b89c6b3c7e5289c75359a36451d0f56faa82
nrrao <nandanarao@yahoo.com>
2019-08-12 16:16:14 -0400

    yearofbirth

--- nandanaFunctionDrills1.js
+++ nandanaFunctionDrills1.js
@@ -1,5 +1,6 @@
 function createGreeting(name,age) {
-    return `Hi ,my name is ${name} and I'm ${age} years old`;
+    const yearOfBirth = 2019-age;
+    return `Hi ,my name is ${name} and I'm ${age} years old and I was born in year ${yearOfBirth}`;
 }
 const greeting1 = createGreeting("nandana",29);
 console.log(greeting1);
