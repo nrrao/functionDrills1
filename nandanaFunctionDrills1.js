d09a07b2b2b158c62d5af31dff7dcb1157e57842
nrrao <nandanarao@yahoo.com>
2019-08-12 17:02:16 -0400

    try and catch

--- nandanaFunctionDrills1.js
+++ nandanaFunctionDrills1.js
@@ -1,10 +1,24 @@
-function getYearOfBirth(age){
-    return 2019-age;
+// eslint-disable-next-line strict
+function yearOfBirth(age) {
+  if(Math.sign(age) === -1){
+    throw new Error('Age can not be negative');
+  }
+  return 2019-age;
 }
+
+try {
+  // eslint-disable-next-line no-unused-vars
+  const greeting1 = createGreeting('nandana',-29); 
+} catch(err){
+  // eslint-disable-next-line no-console
+  console.log(err);
+}
+
+
     
 function createGreeting(name,age) {
-    const yob = getYearOfBirth(age);
-    return `Hi ,my name is ${name} and I'm ${age} years old and I was born in year ${yob}`;
+  const yob = yearOfBirth(age);
+  return `Hi ,my name is ${name} and I'm ${age} years old and I was born in year ${yob}`;
 }
-const greeting1 = createGreeting("nandana",29);
+const greeting1 = createGreeting('nandana',29);
 console.log(greeting1);
